b13ce02aa804c053769a58e278e21f4500b16ed1
A. T <alaa@live.cl>
2020-06-22 22:46:33 +0200

    Fixing the setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,13 @@ setup(
     name='easyEstimate',
     version='0.4.2',
     packages=[''],
+    package_data={'': ['Appartement_Estimation_2019_Cat_Boost.joblib', 'repertoire_code_adr.csv']},
+    include_package_data=True,
     url='api.telecomazur.fr',
     license='Apache 2.0',
     author='A. Tounsadi',
     author_email='info@telecomazur.fr',
     description='Estimate habitation in France easily'
 )
+
+#To create the package run python setup.py bdist_wheel

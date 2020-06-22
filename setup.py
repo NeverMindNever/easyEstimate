8371133b3e0711e7359eb62fda536e14a33a44f5
A. T <alaa@live.cl>
2020-06-22 22:40:39 +0200

    Fixing the setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name='easyEstimate',
     version='0.4.2',
-    packages=['easyEstimate'],
+    packages=[''],
     url='api.telecomazur.fr',
     license='Apache 2.0',
     author='A. Tounsadi',
     author_email='info@telecomazur.fr',
-    description='An estimator for price of habitations in France'
+    description='Estimate habitation in France easily'
 )
